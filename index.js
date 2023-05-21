7205c714c16f5f272a7ca5ebb2a7927ee12dd67b
birdmeister <>
2023-05-21 11:39:59 +0200

    Step 9: pug template, GET route for /update-cobj

--- index.js
+++ index.js
@@ -8,15 +8,35 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please include the private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-eu1-b1c62847-26b2-49d5-8e05-74b051777db5';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+app.get('/update-cobj', async (req, res) => {
+    const objectType = "2-114418761";
+    const objectId = "1253467615";
+    const pageTitle = "Update Custom Object Form | Integrating With HubSpot I Practicum";
 
-// * Code for Route 2 goes here
+    const getSong = `https://api.hubapi.com/crm/v3/objects/${objectType}/${objectId}?properties=name,release_date,length,average_rating,spotify_plays`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const response = await axios.get(getSong, { headers });
+        const data = response.data;
+
+        // res.json(data);
+        res.render('updates', { pageTitle: pageTitle, name: data.properties.name, releaseDate: data.properties.release_date, length: data.properties.length, averageRating: data.properties.average_rating, spotifyPlays: data.properties.spotify_plays });
+
+    } catch (err) {
+        console.error(err);
+    }
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
