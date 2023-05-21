9e260d389f1a1ed494c55d85e3333949e73e8ce9
birdmeister <>
2023-05-21 12:30:24 +0200

    Stap 10 completed
    Update completed. Not sure yet how to do Create

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PRIVATE_APP_ACCESS = 'pat-eu1-b1c62847-26b2-49d5-8e05-74b051777db5';
 
 // * Code for Route 1 goes here
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+// * Code for Route 2 goes here
 app.get('/update-cobj', async (req, res) => {
     const objectType = "2-114418761";
     const objectId = "1253467615";
@@ -38,54 +38,33 @@ app.get('/update-cobj', async (req, res) => {
     }
 });
 
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
 // * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
+app.post('/update-cobj', async (req, res) => {
     const update = {
         properties: {
-            "favorite_book": req.body.newVal
+            "name": req.body.name,
+            "length": req.body.length,
+            "average_rating": req.body.average_rating
         }
     }
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+    const objectType = "2-114418761";
+    const objectId = "1253467615";
+
+    const updateSong = `https://api.hubapi.com/crm/v3/objects/${objectType}/${objectId}`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
+    try {
+        await axios.patch(updateSong, update, { headers });
         res.redirect('back');
-    } catch(err) {
+    } catch (err) {
         console.error(err);
     }
 
 });
-*/
-
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
